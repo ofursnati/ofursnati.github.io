014865b7abf0bb6ec4af18cda22655944ec63118
ofursnati <54673766+ofursnati@users.noreply.github.com>
2020-03-24 22:04:32 +0000

    Add files via upload

---
+++ precache-manifest.6404fbce7809b38ea8e0195fe4b17c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6987ff199c2c1c374fb140422f3c85b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b51941ddd4b82cf832d3",
+    "url": "/static/css/main.16ecacec.chunk.css"
+  },
+  {
+    "revision": "b5c8ab70906f30e12179",
+    "url": "/static/js/2.55091029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55091029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51941ddd4b82cf832d3",
+    "url": "/static/js/main.421f6a05.chunk.js"
+  },
+  {
+    "revision": "4237ffea1ed470a84825",
+    "url": "/static/js/runtime-main.c6df1024.js"
+  }
+]);
