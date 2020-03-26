5fe48ca2b900dd28e89e5f9bd0d92a89f901381a
ofursnati <54673766+ofursnati@users.noreply.github.com>
2020-03-26 17:25:35 +0000

    Add files via upload
    click on bundles to show/hide

---
+++ precache-manifest.1b8b8cf1fcfa1cfa080790127490af13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f65b064398e18aafa7b62f6c4e2cda5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495e6b8cc0eecb9d1321",
+    "url": "/static/css/main.1f32aaf7.chunk.css"
+  },
+  {
+    "revision": "b5c8ab70906f30e12179",
+    "url": "/static/js/2.55091029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55091029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495e6b8cc0eecb9d1321",
+    "url": "/static/js/main.ae0336db.chunk.js"
+  },
+  {
+    "revision": "4237ffea1ed470a84825",
+    "url": "/static/js/runtime-main.c6df1024.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.983b7cb61a258e71e6ed5b577fa7a94c.js"
+  "/precache-manifest.1b8b8cf1fcfa1cfa080790127490af13.js"
 );
 
 self.addEventListener('message', (event) => {
